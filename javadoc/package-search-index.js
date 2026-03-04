7a956989902adc6fd2381e3688e6a2019d961e40
henryr20 <39571531+henryr20@users.noreply.github.com>
2026-03-04 19:15:32 +0100

    Javadoc

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"a403"},{"l":"All Packages","u":"allpackages-index.html","k":"18"}];updateSearchResults();
---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"a403","l":"GestorTareas"},{"p":"a403","l":"Main"},{"p":"a403","l":"Tarea"}];updateSearchResults();
