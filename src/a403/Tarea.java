f89b27221e26c45c476ad4ca5aa2e7efacf589a0
henryr20 <39571531+henryr20@users.noreply.github.com>
2026-03-04 15:45:10 +0000

    Estructura inicial: Clase Tarea.

---
+++ src/a403/Tarea.java
@@ -0,0 +1,28 @@
+package a403;
+
+public class Tarea {
+
+    private String descripcion;
+    private boolean completada;
+
+    public Tarea(String descripcion) {
+        this.descripcion = descripcion;
+        this.completada = false;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public boolean isCompletada() {
+        return completada;
+    }
+
+    public void setCompletada(boolean completada) {
+        this.completada = completada;
+    }
+
+    public String toString() {
+        return (completada ? "[X] " : "[ ] ") + descripcion;
+    }
+}
